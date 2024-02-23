c894ca36c026bce8168667c6c3bee96a3b0bae8a
valsv0602 <104919819+valsv0602@users.noreply.github.com>
2024-02-23 17:06:26 -0600

    Primer commit

---
+++ src/main/java/com/practica01/Practica1Grupo2MnApplication.java
@@ -0,0 +1,13 @@
+package com.practica01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica1Grupo2MnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica1Grupo2MnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practica01/Practica1Grupo2MnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica1Grupo2MnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
