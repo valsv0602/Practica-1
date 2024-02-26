a47dfbaa8588ccb4836d0f531b1beb48cfc24737
valsv0602 <104919819+valsv0602@users.noreply.github.com>
2024-02-26 11:57:14 -0600

    front end

--- src/main/resources/static/js/rutinas.js
+++ src/main/resources/static/js/rutinas.js
@@ -1,9 +1,11 @@
 function readURL(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-    reader.onload = function (e) {
-      $("blah").attr("src", e.target.result).height(200);
-    };
-    reader.readAsDataURL(input.files[0]);
-  }
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#blah')
+                    .attr('src', e.target.result)
+                    .height(200);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
 }
